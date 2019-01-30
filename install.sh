e78ea24e3a2496cb77d63c7ab2d4e950716b8b15
André Carneiro <acarneiro@mambowifi.com>
2019-01-30 16:06:55 -0200

    some fixes

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@ PATHDOTFILES="$HOME/.dotfiles"
 
 INSTALLED_MODULES="not yet"
 FAIL_MSG="!!!WARNING!!!\n\nFailed installing modules\nIf you want to continue anyway run:\n\ttouch \"\$HOME/.dotfiles/install_lock\"\n"
-echo "sudo apt-get install $( cat "$PATHDOTFILES/install_requires.txt" | tr '\r\n' ' ' )"  && INSTALLED_MODULES="ok" && echo -e $FAIL_MSG
+sudo apt-get install $( cat "$PATHDOTFILES/install_requires.txt" | tr '\r\n' ' ' )  && INSTALLED_MODULES="ok" || echo -e $FAIL_MSG
 
-if [ $INSTALLED_MODULES = "ok" ]; then
-	echo "Modules were installed";
+if [ "$INSTALLED_MODULES" = "ok" ]; then
 	touch "$PATHDOTFILES/install_lock"
+	echo -e "\nModules were installed successfully\nRun setup.sh to continue..."
 else
-	echo "Please install modules to continue";
+	echo "Please install modules to continue"
 fi
